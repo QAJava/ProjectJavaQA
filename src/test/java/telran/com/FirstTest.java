31da6c7ed150e8561c0e5300052dec26e66229d7
yacov <jakoff@gmail.com>
2015-08-19 20:57:36 +0300

    Initial commit

---
+++ src/test/java/telran/com/FirstTest.java
@@ -0,0 +1,104 @@
+package telran.com;
+
+/**
+ * Created by Iakov Volf on 8/19/2015.
+ */
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class FirstTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://kontur.ru/Files/userfiles/file/edu/Stagirovka%202012/test/default.html";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testMenu721prichislov() throws Exception {
+        driver.get("https://kontur.ru/Files/userfiles/file/edu/Stagirovka%202012/test/default.html");
+        new Select(driver.findElement(By.id("days"))).selectByVisibleText("пятница");
+        driver.findElement(By.xpath("(//input[@type='checkbox'])[5]")).click();
+        driver.findElement(By.xpath("(//input[@type='checkbox'])[6]")).click();
+        driver.findElement(By.xpath("(//input[@type='checkbox'])[8]")).click();
+        driver.findElement(By.id("makeOrder")).click();
+        try {
+            assertTrue(isElementPresent(By.xpath("//*[@id='history']/li[last()][contains(text(),'пятница')]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(isElementPresent(By.xpath("//*[@id='history']/li[last()][contains(text(),'салат мистерия весны')]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(isElementPresent(By.xpath("//*[@id='history']/li[last()][contains(text(),'лагман')]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(isElementPresent(By.xpath("//*[@id='history']/li[last()][contains(text(),' капуста жареная. Списано с личного счета 125 р.')]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @AfterTest
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
+
