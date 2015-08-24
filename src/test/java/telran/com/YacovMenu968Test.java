fb7cd23742756099232c4ab69d0dd8cde31e958d
yacov <jakoff@gmail.com>
2015-08-24 22:23:42 +0300

    new correct class name

--- src/test/java/telran/com/YacovMenu968Test.java
+++ src/test/java/telran/com/YacovMenu968Test.java
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.fail;
 import static org.testng.AssertJUnit.assertTrue;
 
-public class Menu968Test {
+public class YacovMenu968Test {
 private WebDriver driver;
 private String baseUrl;
 private boolean acceptNextAlert = true;
