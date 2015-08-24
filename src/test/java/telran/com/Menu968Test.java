b58bdb40f569827fc0eec59b9ee31b28d365740e
yacov <jakoff@gmail.com>
2015-08-24 22:09:20 +0300

    New test added 968

---
+++ src/test/java/telran/com/Menu968Test.java
@@ -0,0 +1,88 @@
+package telran.com;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class Menu968Test {
+private WebDriver driver;
+private String baseUrl;
+private boolean acceptNextAlert = true;
+private StringBuffer verificationErrors = new StringBuffer();
+
+@BeforeTest
+public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://kontur.ru/Files/userfiles/file/edu/Stagirovka%202012/test/default.html";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        }
+
+@Test
+public void test986Yacov() throws Exception {
+        driver.get("https://kontur.ru/Files/userfiles/file/edu/Stagirovka%202012/test/default.html");
+        new Select(driver.findElement(By.id("days"))).selectByVisibleText("вторник");
+        driver.findElement(By.xpath("//select[@id='days']/option[3]")).click();
+        driver.findElement(By.xpath("(//input[@type='checkbox'])[5]")).click();
+        driver.findElement(By.xpath("(//input[@type='checkbox'])[6]")).click();
+        driver.findElement(By.id("makeOrder")).click();
+        try {
+        assertTrue(isElementPresent(By.xpath("//*[@id='history']/li[last()][contains(text(),'163')]")));
+        } catch (Error e) {
+        verificationErrors.append(e.toString());
+        }
+        }
+
+@AfterTest
+public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+        fail(verificationErrorString);
+        }
+        }
+
+private boolean isElementPresent(By by) {
+        try {
+        driver.findElement(by);
+        return true;
+        } catch (NoSuchElementException e) {
+        return false;
+        }
+        }
+
+private boolean isAlertPresent() {
+        try {
+        driver.switchTo().alert();
+        return true;
+        } catch (NoAlertPresentException e) {
+        return false;
+        }
+        }
+
+private String closeAlertAndGetItsText() {
+        try {
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        if (acceptNextAlert) {
+        alert.accept();
+        } else {
+        alert.dismiss();
+        }
+        return alertText;
+        } finally {
+        acceptNextAlert = true;
+        }
+        }
+        }
