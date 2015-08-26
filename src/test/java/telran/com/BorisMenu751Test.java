ebead203511e6f829ac389416cf7335035ec60c1
BorisSN <boris.snigirev@gmail.com>
2015-08-26 18:17:58 +0300

    Added 2 DalyMenuTest 750, 751

---
+++ src/test/java/telran/com/BorisMenu751Test.java
@@ -0,0 +1,80 @@
+package telran.com;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class BorisMenu751Test {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "http://change-this-to-the-site-you-are-testing/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testBorisMenu751() throws Exception {
+        driver.get("https://kontur.ru/Files/userfiles/file/edu/Stagirovka%202012/test/default.html");
+        new Select(driver.findElement(By.id("days"))).selectByVisibleText("суббота");
+        driver.findElement(By.xpath("(//input[@type='checkbox'])[6]")).click();
+        driver.findElement(By.xpath("(//input[@type='checkbox'])[7]")).click();
+        driver.findElement(By.xpath("//*[@id='makeOrder']")).click();
+        try {
+            assertTrue(isElementPresent(By.xpath("//*[@id='history']/li[last()][contains(text(),'Списано с личного счета 131 р.')]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
