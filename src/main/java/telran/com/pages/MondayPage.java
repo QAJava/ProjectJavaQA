74fbffe5922a3db700e6bc94ebe1f9291a059ea6
yacov <jakoff@gmail.com>
2015-08-30 21:04:28 +0300

    new classes and methods

--- src/main/java/telran/com/pages/MondayPage.java
+++ src/main/java/telran/com/pages/MondayPage.java
@@ -3,26 +3,27 @@ package telran.com.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
 import java.io.IOException;
 
-public class HomePage extends Page {
+public class MondayPage extends Page {
     public String baseUrl;
 
 
 	private WebElement webElement;
 
-  @FindBy(how = How.TAG_NAME, using = "h1")
-  public WebElement header;
 
     //Days
 
     @FindBy(xpath = "//*[@id='menu']/div[@key='1']/div[1]/input")
     WebElement kashaMannayaMo;
-
+    @FindBy(xpath = "//*[@id='menu']/div[@key='1']/div[2]/input")
+    WebElement yaichnitsaMo;
+    @FindBy(xpath = "//*[@id='menu']/div[@key='1']/div[3]/input")
+    WebElement shvedStolaMo;
+    @FindBy(xpath = "//*[@id='menu']/div[@key='1']/div[4]/input")
+    WebElement plovMo;
 
     @FindBy(id = "days")
 	protected WebElement days;
@@ -30,41 +31,28 @@ public class HomePage extends Page {
     @FindBy(id = "orderSum")
     protected WebElement orderSum;
 
-	@FindBy(id = "makeOrder")
-    protected WebElement makeOrderButton;
-
-	@FindBy(xpath = "//*[@id='history']/li[last()]")
-    protected WebElement currentOrderHistory;
-
-	@FindBy(linkText = "Начать заново")
-    protected WebElement startOverLink;
-
-	@FindBy(id = "account")
-	private WebElement accountSum;
 
 
-    public HomePage(WebDriver driver) {
+    public MondayPage(WebDriver driver) {
         super(driver);
         this.PAGE_URL = baseUrl;
         PageFactory.initElements(driver, this);
     }
 
-    public HomePage openHomePage() {
+    public MondayPage openHomePage() {
         driver.get(PAGE_URL);
         return this;
     }
 
-public HomePage clickToMakeOrder (){
-    clickElement(makeOrderButton);
+public MondayPage clickToCheckboxKasha(){
+    kashaMannayaMo.click();
     return this;
 }
 
-
-    public HomePage selectDay (String day){
-        new Select(days).selectByVisibleText(day);
+    public MondayPage clickToEggs(){
+        yaichnitsaMo.click();
         return this;
     }
-
     public int getOrderSum () {
         int ordersum = 0;
        ordersum = Integer.parseInt(orderSum.getText());
--- src/test/java/telran/com/YacovMenu968Test.java
+++ src/test/java/telran/com/YacovMenu968Test.java
@@ -5,43 +5,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import telran.com.pages.HomePage;
+import telran.com.pages.MondayPage;
 
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
-import static org.testng.AssertJUnit.assertTrue;
 
 public class YacovMenu968Test {
 private WebDriver driver;
 private String baseUrl;
 private boolean acceptNextAlert = true;
 private StringBuffer verificationErrors = new StringBuffer();
+        public HomePage homePage;
+        public MondayPage mondayPage;
 
 @BeforeTest
 public void setUp() throws Exception {
         driver = new FirefoxDriver();
         baseUrl = "https://kontur.ru/Files/userfiles/file/edu/Stagirovka%202012/test/default.html";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        homePage = PageFactory.initElements(driver, HomePage.class);
+        mondayPage = PageFactory.initElements(driver, MondayPage.class);
         }
 
 @Test
-public void test986Yacov() throws Exception {
+public void test986Yacov()  {
         driver.get(baseUrl);
-        new Select(driver.findElement(By.id("days"))).selectByVisibleText("вторник");
-       // driver.findElement(By.xpath("//select[@id='days']/option[3]")).click();
-        driver.findElement(By.xpath("(//input[@type='checkbox'])[5]")).click();
-        driver.findElement(By.xpath("(//input[@type='checkbox'])[6]")).click();
-        driver.findElement(By.id("makeOrder")).click();
-        try {
-        assertTrue(isElementPresent(By.xpath("//*[@id='history']/li[last()][contains(text(),'163')]")));
-        } catch (Error e) {
-        verificationErrors.append(e.toString());
-        }
+        homePage.selectDay("понедельник");
+        mondayPage.clickToEggs();
+        Assert.assertEquals (homePage.getOrderSum(),70);
+
         }
 
 @AfterTest
