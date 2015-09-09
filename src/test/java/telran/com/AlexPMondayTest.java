0f992f1571858f8b4f45d6534d46f0d4921da878
alexp7 <orad.qc@gmail.com>
2015-09-09 18:24:07 +0300

    update for class Menu973

---
+++ src/test/java/telran/com/AlexPMondayTest.java
@@ -0,0 +1,85 @@
+package telran.com;
+
+        import java.util.regex.Pattern;
+        import java.util.concurrent.TimeUnit;
+
+        import org.openqa.selenium.support.PageFactory;
+        import org.testng.*;
+        import org.testng.annotations.AfterTest;
+        import org.testng.annotations.BeforeTest;
+        import org.testng.annotations.Test;
+        import static org.testng.Assert.*;
+        import static org.hamcrest.CoreMatchers.*;
+        import org.openqa.selenium.*;
+        import org.openqa.selenium.firefox.FirefoxDriver;
+        import org.openqa.selenium.support.ui.Select;
+        import telran.com.pages.AlexPMondayPage;
+
+public class AlexPMondayTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+    public AlexPMondayPage mondayPage;
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://kontur.ru/Files/userfiles/file/edu/Stagirovka%202012/test/default.html";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        mondayPage = PageFactory.initElements(driver,AlexPMondayPage.class);
+    }
+
+    @Test
+    public void testMenuAlexP() {
+        driver.get(baseUrl);
+        mondayPage.selectDay("понедельник");
+        mondayPage.clickToKasha();
+        mondayPage.clickToPlov();
+        mondayPage.clickToMakeOrder();
+        mondayPage.checkTextInReport(driver,"152");
+
+    }
+
+    @AfterTest
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
